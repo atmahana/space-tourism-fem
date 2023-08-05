30d690a3ac074c0e79dc1aaa43f56961fa910fe0
Adham <zubairadham.ar@gmail.com>
2023-08-05 22:36:37 +0800

    fixed destination image change when tab changes

--- src/components/Tabs/Destination/DestinationTabs.jsx
+++ src/components/Tabs/Destination/DestinationTabs.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import ImageMoon from "../../../assets/destination/image-moon.webp";
+import ImageMars from "../../../assets/destination/image-mars.webp";
+import ImageEuropa from "../../../assets/destination/image-europa.webp";
+import ImageTitan from "../../../assets/destination/image-titan.webp";
+
+const images = [ImageMoon, ImageMars, ImageEuropa, ImageTitan];
 
 function DestinationTabs({ destinations }) {
+  const [tabIndex, setTabIndex] = useState(0);
+
   return (
-    <Tabs className="mt-7">
-      <TabList className="flex justify-center gap-6">
+    <Tabs
+      className="mt-7"
+      selectedIndex={tabIndex}
+      onSelect={(index) => setTabIndex(index)}
+    >
+      <img
+        className="w-[10.625rem] h-[10.625rem] m-auto mb-6"
+        src={images[tabIndex]}
+      />
+      <TabList className="flex justify-center gap-[1.625rem]">
         {destinations.map((destination) => (
           <Tab
             key={destination.name}
-            className="text-secondary uppercase font-sans-condensed tracking-235 text-sm"
+            selectedClassName="text-white border-b-[3px]"
+            className="text-secondary uppercase font-sans-condensed tracking-235 text-sm h-[1.875rem]"
           >
             {destination.name}
           </Tab>
@@ -15,19 +33,16 @@ function DestinationTabs({ destinations }) {
       </TabList>
 
       {destinations.map((destination) => (
-        <TabPanel
-          key={destination.name}
-          className="grid divide-y divide-[#383B4B] px-6"
-        >
-          <div className="text-center py-[2.0625rem]">
-            <h2 className="leading-none text-white text-3xl uppercase">
+        <TabPanel key={destination.name} className="grid px-6">
+          <div className="text-center pt-4 pb-8 border-b border-[#383B4B]">
+            <h2 className="leading-tight text-white text-3xl uppercase">
               {destination.name}
             </h2>
             <p className="leading-[1.5625rem] text-[0.9375rem] text-secondary">
               {destination.description}
             </p>
           </div>
-          <div className="grid gap-8 py-[2.0625rem]">
+          <div className="flex flex-col gap-8 py-[2.0625rem]">
             <div className="grid gap-3">
               <h3 className="text-secondary text-sm font-sans-condensed tracking-235 leading-none">
                 AVG. DISTANCE
--- src/pages/Destination/Index.jsx
+++ src/pages/Destination/Index.jsx
@@ -1,4 +1,3 @@
-import ImageMoon from "../../assets/destination/image-moon.webp";
 import DestinationTabs from "../../components/Tabs/Destination/DestinationTabs";
 import data from "../../data.json";
 
@@ -6,17 +5,14 @@ function DestinationPage() {
   return (
     <section className="bg-dest-mobile bg-cover h-full text-center overflow-y-scroll">
       <div className="pt-[88px] flex flex-col items-center">
-        <div className="grid gap-8">
-          <div className="flex gap-[1.125rem] leading-none text-white tracking-270">
-            <span className="font-sans-condensed opacity-25 font-bold">01</span>
-            <h1 className="text-base font-sans-condensed ">
-              PICK YOUR DESTINATION
-            </h1>
-          </div>
-          <img className="w-[10.625rem] h-[10.625rem] m-auto" src={ImageMoon} />
+        <div className="flex gap-[1.125rem] leading-none text-white tracking-270 items-center pb-2">
+          <span className="font-sans-condensed opacity-25 font-bold">01</span>
+          <h1 className="text-base font-sans-condensed ">
+            PICK YOUR DESTINATION
+          </h1>
         </div>
         <div>
-          <DestinationTabs destinations={data.destinations}/>
+          <DestinationTabs destinations={data.destinations} />
         </div>
       </div>
     </section>
