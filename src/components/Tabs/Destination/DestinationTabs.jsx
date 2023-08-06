1f3444b8f01f48a654a7b5dc4b20f45f68b56a26
Adham <zubairadham.ar@gmail.com>
2023-08-06 21:46:13 +0800

    [T] destination page

--- src/components/Tabs/Destination/DestinationTabs.jsx
+++ src/components/Tabs/Destination/DestinationTabs.jsx
@@ -12,20 +12,20 @@ function DestinationTabs({ destinations }) {
 
   return (
     <Tabs
-      className="mt-7"
+      className="mt-7 md:mt-14"
       selectedIndex={tabIndex}
       onSelect={(index) => setTabIndex(index)}
     >
       <img
-        className="w-[10.625rem] h-[10.625rem] m-auto mb-6"
+        className="w-[10.625rem] h-[10.625rem] md:max-xl:w-[18.75rem] md:max-xl:h-[18.75rem] m-auto mb-6 md:mb-12"
         src={images[tabIndex]}
       />
-      <TabList className="flex justify-center gap-[1.625rem]">
+      <TabList className="flex justify-center gap-[1.625rem] md:gap-[2.125rem]">
         {destinations.map((destination) => (
           <Tab
             key={destination.name}
             selectedClassName="text-white border-b-[3px]"
-            className="text-secondary uppercase font-sans-condensed tracking-235 text-sm h-[1.875rem]"
+            className="text-secondary uppercase font-sans-condensed tracking-235 md:tracking-270 text-sm md:text-base h-[1.875rem] md:h-[2.375rem]"
           >
             {destination.name}
           </Tab>
@@ -33,17 +33,17 @@ function DestinationTabs({ destinations }) {
       </TabList>
 
       {destinations.map((destination) => (
-        <TabPanel key={destination.name} className="grid px-6">
-          <div className="text-center pt-4 pb-8 border-b border-[#383B4B]">
-            <h2 className="leading-tight text-white text-3xl uppercase">
+        <TabPanel key={destination.name} className="grid px-6 md:px-14">
+          <div className="text-center pt-4 pb-8 md:pb-12 border-b border-[#383B4B]">
+            <h2 className="leading-tight md:leading-none md:pt-5 uppercase text-white text-3xl md:text-4xl">
               {destination.name}
             </h2>
-            <p className="leading-[1.5625rem] text-[0.9375rem] text-secondary">
+            <p className="leading-[1.5625rem] md:leading-7 md:pt-4 text-[0.9375rem] md:text-base text-secondary">
               {destination.description}
             </p>
           </div>
-          <div className="flex flex-col gap-8 py-[2.0625rem]">
-            <div className="grid gap-3">
+          <div className="flex flex-col md:flex-row md:justify-center gap-8 md:gap-3 py-[2.0625rem] md:py-[1.875rem]">
+            <div className="grid gap-3 md:w-[13.5rem]">
               <h3 className="text-secondary text-sm font-sans-condensed tracking-235 leading-none">
                 AVG. DISTANCE
               </h3>
@@ -51,7 +51,7 @@ function DestinationTabs({ destinations }) {
                 {destination.distance}
               </p>
             </div>
-            <div className="grid gap-3">
+            <div className="grid gap-3 md:w-[13.9375rem]">
               <h3 className="text-secondary text-sm font-sans-condensed tracking-235 leading-none">
                 EST. TRAVEL TIME
               </h3>
--- src/pages/Destination/Index.jsx
+++ src/pages/Destination/Index.jsx
@@ -3,11 +3,13 @@ import data from "../../data.json";
 
 function DestinationPage() {
   return (
-    <section className="bg-dest-mobile bg-cover h-full text-center overflow-y-scroll">
-      <div className="pt-[88px] flex flex-col items-center">
-        <div className="flex gap-[1.125rem] leading-none text-white tracking-270 items-center pb-2">
-          <span className="font-sans-condensed opacity-25 font-bold">01</span>
-          <h1 className="text-base font-sans-condensed ">
+    <section className="bg-dest-mobile md:max-xl:bg-dest-tablet bg-cover h-full text-center overflow-y-scroll">
+      <div className="pt-[88px] md:max-xl:pt-[136px] flex flex-col items-center md:items-start md:max-xl:px-[2.375rem]">
+        <div className="flex gap-4 leading-none text-white tracking-270 md:tracking-[0.21125rem] items-center pb-1">
+          <span className="font-sans-condensed opacity-25 font-bold md:max-xl:text-[1.25rem] ">
+            01
+          </span>
+          <h1 className="text-base md:max-xl:text-[1.25rem] leading-tight font-sans-condensed ">
             PICK YOUR DESTINATION
           </h1>
         </div>
