e16974a8ae943691218da05ba4872d3701e6ceab
Adham <zubairadham.ar@gmail.com>
2023-08-07 19:58:18 +0800

    [D] destination page

--- src/components/Tabs/Destination/DestinationTabs.jsx
+++ src/components/Tabs/Destination/DestinationTabs.jsx
@@ -1,31 +1,23 @@
-import { useState } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import ImageMoon from "../../../assets/destination/image-moon.webp";
-import ImageMars from "../../../assets/destination/image-mars.webp";
-import ImageEuropa from "../../../assets/destination/image-europa.webp";
-import ImageTitan from "../../../assets/destination/image-titan.webp";
 
-const images = [ImageMoon, ImageMars, ImageEuropa, ImageTitan];
+function DestinationTabs({ destinations, tabIndex, onSelectHandler }) {
 
-function DestinationTabs({ destinations }) {
-  const [tabIndex, setTabIndex] = useState(0);
+  const selectHandler = (index) => {
+    onSelectHandler(index);
+  }
 
   return (
     <Tabs
-      className="mt-7 md:mt-14"
+      className="mt-6 md:mt-0 xl:self-start"
       selectedIndex={tabIndex}
-      onSelect={(index) => setTabIndex(index)}
+      onSelect={selectHandler}
     >
-      <img
-        className="w-[10.625rem] h-[10.625rem] md:max-xl:w-[18.75rem] md:max-xl:h-[18.75rem] m-auto mb-6 md:mb-12"
-        src={images[tabIndex]}
-      />
-      <TabList className="flex justify-center gap-[1.625rem] md:gap-[2.125rem]">
+      <TabList className="flex justify-center xl:justify-start gap-[1.625rem] md:max-xl:gap-[2.125rem] xl:gap-[2.125rem]">
         {destinations.map((destination) => (
           <Tab
             key={destination.name}
             selectedClassName="text-white border-b-[3px]"
-            className="text-secondary uppercase font-sans-condensed tracking-235 md:tracking-270 text-sm md:text-base h-[1.875rem] md:h-[2.375rem]"
+            className="text-secondary uppercase font-sans-condensed tracking-235 md:tracking-270 text-sm md:text-base h-[1.875rem] md:max-xl:h-[2.375rem] xl:h-9 xl:leading-tight"
           >
             {destination.name}
           </Tab>
@@ -33,17 +25,17 @@ function DestinationTabs({ destinations }) {
       </TabList>
 
       {destinations.map((destination) => (
-        <TabPanel key={destination.name} className="grid px-6 md:px-14">
-          <div className="text-center pt-4 pb-8 md:pb-12 border-b border-[#383B4B]">
-            <h2 className="leading-tight md:leading-none md:pt-5 uppercase text-white text-3xl md:text-4xl">
+        <TabPanel key={destination.name} className="grid px-6 md:px-14 xl:px-0">
+          <div className="text-center xl:text-start pt-4 xl:pt-6 pb-8 md:max-xl:pb-12 xl:pb-14 border-b border-[#383B4B]">
+            <h2 className="leading-tight md:leading-none md:pt-5 uppercase text-white text-3xl md:max-xl:text-4xl xl:text-5xl">
               {destination.name}
             </h2>
-            <p className="leading-[1.5625rem] md:leading-7 md:pt-4 text-[0.9375rem] md:text-base text-secondary">
+            <p className="leading-[1.5625rem] md:max-xl:leading-7 xl:leading-8 md:max-xl:pt-4 xl:pt-5 text-[0.9375rem] md:max-xl:text-base xl:text-lg text-secondary">
               {destination.description}
             </p>
           </div>
-          <div className="flex flex-col md:flex-row md:justify-center gap-8 md:gap-3 py-[2.0625rem] md:py-[1.875rem]">
-            <div className="grid gap-3 md:w-[13.5rem]">
+          <div className="flex flex-col md:flex-row md:max-xl:justify-center gap-8 md:max-xl:gap-3 xl:gap-[4.8rem] py-[2.0625rem] md:py-[1.875rem]">
+            <div className="grid gap-3 md:max-xl:w-[13.5rem] xl:text-start">
               <h3 className="text-secondary text-sm font-sans-condensed tracking-235 leading-none">
                 AVG. DISTANCE
               </h3>
@@ -51,7 +43,7 @@ function DestinationTabs({ destinations }) {
                 {destination.distance}
               </p>
             </div>
-            <div className="grid gap-3 md:w-[13.9375rem]">
+            <div className="grid gap-3 md:max-xl:w-[13.9375rem] xl:text-start">
               <h3 className="text-secondary text-sm font-sans-condensed tracking-235 leading-none">
                 EST. TRAVEL TIME
               </h3>
--- src/pages/Destination/Index.jsx
+++ src/pages/Destination/Index.jsx
@@ -1,20 +1,38 @@
 import DestinationTabs from "../../components/Tabs/Destination/DestinationTabs";
 import data from "../../data.json";
+import { useState } from "react";
+import ImageMoon from "../../assets/destination/image-moon.webp";
+import ImageMars from "../../assets/destination/image-mars.webp";
+import ImageEuropa from "../../assets/destination/image-europa.webp";
+import ImageTitan from "../../assets/destination/image-titan.webp";
+
+const images = [ImageMoon, ImageMars, ImageEuropa, ImageTitan];
 
 function DestinationPage() {
+  const [tabIndex, setTabIndex] = useState(0);
+  const selectHandler = (index) => setTabIndex(index);
+
   return (
-    <section className="bg-dest-mobile md:max-xl:bg-dest-tablet bg-cover h-full text-center overflow-y-scroll">
-      <div className="pt-[88px] md:max-xl:pt-[136px] flex flex-col items-center md:items-start md:max-xl:px-[2.375rem]">
-        <div className="flex gap-4 leading-none text-white tracking-270 md:tracking-[0.21125rem] items-center pb-1">
-          <span className="font-sans-condensed opacity-25 font-bold md:max-xl:text-[1.25rem] ">
+    <section className="bg-dest-mobile md:max-xl:bg-dest-tablet xl:bg-dest-desktop bg-cover h-full text-center overflow-y-scroll">
+      <div className="pt-[88px] md:max-xl:pt-[136px] xl:pt-[13rem] flex flex-col items-center md:items-start md:max-xl:px-[2.375rem] xl:px-[10.375rem]">
+        <div className="flex gap-4 xl:gap-6 leading-none xl:leading-normal text-white tracking-270 md:tracking-[0.21125rem] xl:tracking-475 items-center pb-8 md:max-xl:pb-14 xl:pb-[3.7rem]">
+          <span className="font-sans-condensed opacity-25 font-bold md:max-xl:text-[1.25rem] xl:text-xl">
             01
           </span>
-          <h1 className="text-base md:max-xl:text-[1.25rem] leading-tight font-sans-condensed ">
+          <h1 className="text-base md:max-xl:text-[1.25rem] xl:text-xl leading-tight font-sans-condensed ">
             PICK YOUR DESTINATION
           </h1>
         </div>
-        <div>
-          <DestinationTabs destinations={data.destinations} />
+        <div className="flex flex-col xl:flex-row xl:gap-[9.8rem] xl:ml-16">
+          <img
+            className="w-[10.625rem] h-[10.625rem] md:max-xl:w-[18.75rem] md:max-xl:h-[18.75rem] xl:w-[27.8125rem] xl:h-[27.8125rem] m-auto xl:m-0 mb-0 md:mb-[3.4rem] xl:self-center"
+            src={images[tabIndex]}
+          />
+          <DestinationTabs
+            destinations={data.destinations}
+            tabIndex={tabIndex}
+            onSelectHandler={selectHandler}
+          />
         </div>
       </div>
     </section>
